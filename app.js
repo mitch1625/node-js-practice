3413b371d91babdced046bfb01624c6dd9a29100
eric <ericmitchell1625@hotmail.com>
2024-01-11 16:11:34 -0500

    1st nodejs assignment

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const http = require('http') // import http module
-const fs = require('fs');
-const { parse } = require('path');
-
 // createSever has a RequestListener argument that is a function that executes at every incoming request
 // RequestListener requires two args : request and response 
 // function rqListener(req, res) {
@@ -18,41 +14,18 @@ const { parse } = require('path');
 // can use arrow syntax
 // assign server to a variable access it
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title><>Enter Message</title></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end()
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = []
-    req.on('data', (chunk) => {
-      console.log(chunk)
-      body.push(chunk)
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody)
-      const message = parsedBody.split('=')[1]
-      fs.writeFileSync('message.txt', message)
-    })
+const http = require('http') // import http module
 
-    res.statusCode = 302;
-    res.setHeader('Location', '/')
-    return res.end()
-  }
+const routes = require('./routes')
 
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>');
-  res.write('<head><title><>my first page</title></head>')
-  res.write('<body><h1>hello my first Node.js server</h1></body>')
-  res.write('</html>')
-  res.end()
-})
+
+const server = http.createServer(routes)
+
+//  if modules exported as object:
+//  const server = http.createServer(routes.handler)
+//  console.log(routes.someText)
+
+
 
 server.listen(5173) // multiple args you can pass in
 // listen method will keep server running for incoming requests
---
+++ routes.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const { formatWithOptions } = require('util');
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title><>Enter Message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = []
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk)
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody)
+      const message = parsedBody.split('=')[1]
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end()
+      })
+    })
+  
+  }
+  
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>');
+  res.write('<head><title><>my first page</title></head>')
+  res.write('<body><h1>hello my first Node.js server</h1></body>')
+  res.write('</html>')
+  
+  res.end()
+}
+
+module.exports = requestHandler
+// can also export written as object
+/**
+ module.exports = {
+   handler : requestHandler,
+   someText: 'Some hard coded text'
+ }
+
+
+written individually but considered one export 
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text'
+
+
+*/
